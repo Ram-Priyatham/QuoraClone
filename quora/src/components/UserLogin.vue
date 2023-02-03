576ffe461d07a5dba2790b71c541cab7ec6c4440
Ram Priyatham <kolisettyrampriyatham@gmail.com>
2023-02-03 14:54:49 +0530

    Registration and Login changes done

--- quora/src/components/UserLogin.vue
+++ quora/src/components/UserLogin.vue
@@ -1,27 +1,33 @@
 <template>
-  <div class="container">
-    <h2 class="text-center">Login</h2>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          v-model="username"
-        />
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          v-model="password"
-        />
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <div class="bg">
+    <div class="container">
+      <img
+        src="../assets/quora-ar21-removebg-preview.png"
+        style="width: 250px"
+      />
+      <h2 class="text-center">Login</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            v-model="username"
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -51,10 +57,12 @@ export default {
 </script>
 <style scoped>
 .container {
+  /* display: flex; */
   max-width: 500px;
   margin: 0 auto;
   padding: 30px;
   text-align: center;
+  background-color: rgb(236, 241, 241);
 }
 
 h2 {
@@ -68,7 +76,12 @@ label {
 .form-control {
   margin-bottom: 15px;
 }
-
+.bg {
+  /* display: flex; */
+  background-image: url("https://qph.cf2.quoracdn.net/main-qimg-b5faebde65208b4e09af0a85b3893ddd-lq");
+  width: 100%;
+  height: 100vh;
+}
 .btn {
   width: 100%;
 }
--- quora/src/components/UserRegistration.vue
+++ quora/src/components/UserRegistration.vue
@@ -1,112 +1,118 @@
 <template>
-  <div class="container">
-    <h2 class="text-center">User Registration</h2>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label for="username">Username</label>
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          v-model="username"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="password"
-          v-model="password"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="firstName">First Name</label>
-        <input
-          type="text"
-          class="form-control"
-          id="firstName"
-          v-model="firstName"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="lastName">Last Name</label>
-        <input
-          type="text"
-          class="form-control"
-          id="lastName"
-          v-model="lastName"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="phoneNumber">Phone Number</label>
-        <input
-          type="text"
-          class="form-control"
-          id="phoneNumber"
-          v-model="phoneNumber"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          v-model="email"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="dob">Date of Birth</label>
-        <input
-          type="text"
-          class="form-control"
-          id="dob"
-          v-model="dob"
-          required
-        />
-      </div>
-      <div class="form-group">
-        <label for="platformId">Platform ID</label>
-        <input
-          type="text"
-          class="form-control"
-          id="platformId"
-          v-model="platformId"
-          required
-        />
-      </div>
-      <div class="form-group form-check">
-        <input
-          type="checkbox"
-          class="form-check-input"
-          id="isPrivate"
-          v-model="isPrivate"
-        />
-        <label class="form-check-label" for="isPrivate">Is private?</label>
-      </div>
-      <div class="form-group">
-        <label for="categories">Categories</label>
-        <select
-          multiple
-          class="form-control"
-          id="categories"
-          v-model="categories"
-        >
-          <option v-for="category in categoriesList" :key="category">
-            {{ category }}
-          </option>
-        </select>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-      <!-- <button type="button" class="btn btn-secondary">Reset</button> -->
-    </form>
+  <div class="bg">
+    <div class="container">
+      <img
+        src="../assets/quora-ar21-removebg-preview.png"
+        style="width: 250px"
+      />
+      <h2 class="text-center">User Registration</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            v-model="username"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="firstName">First Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="firstName"
+            v-model="firstName"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="lastName">Last Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="lastName"
+            v-model="lastName"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="phoneNumber">Phone Number</label>
+          <input
+            type="text"
+            class="form-control"
+            id="phoneNumber"
+            v-model="phoneNumber"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            v-model="email"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="dob">Date of Birth</label>
+          <input
+            type="text"
+            class="form-control"
+            id="dob"
+            v-model="dob"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="platformId">Platform ID</label>
+          <input
+            type="text"
+            class="form-control"
+            id="platformId"
+            v-model="platformId"
+            required
+          />
+        </div>
+        <div class="form-group form-check">
+          <input
+            type="checkbox"
+            class="form-check-input"
+            id="isPrivate"
+            v-model="isPrivate"
+          />
+          <label class="form-check-label" for="isPrivate">Is private?</label>
+        </div>
+        <div class="form-group">
+          <label for="categories">Categories</label>
+          <select
+            multiple
+            class="form-control"
+            id="categories"
+            v-model="categories"
+          >
+            <option v-for="category in categoriesList" :key="category">
+              {{ category }}
+            </option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        <!-- <button type="button" class="btn btn-secondary">Reset</button> -->
+      </form>
+    </div>
   </div>
 </template>
 
@@ -164,6 +170,7 @@ export default {
   margin: 0 auto;
   padding: 30px;
   text-align: center;
+  background-color: rgb(236, 241, 241);
 }
 
 h2 {
@@ -177,7 +184,9 @@ label {
 .form-control {
   margin-bottom: 15px;
 }
-
+.bg {
+  background-image: url("https://qph.cf2.quoracdn.net/main-qimg-b5faebde65208b4e09af0a85b3893ddd-lq");
+}
 .form-check-label {
   font-weight: normal;
 }
